db45d3497b4777a8975509cf0f8ed0d6d3676d07
Denis <denis_jr2001@hotmail.com>
2022-06-16 21:03:14 -0300

    struct made

--- map-posicao/src/App.jsx
+++ map-posicao/src/App.jsx
@@ -1,12 +1,31 @@
-import Maps from "./Maps";
-import SearchBox from "./SearchBox";
+import React from "react";
+import MapPosicao from "./MapPosicao/MapPosicao";
+import FormPosicao from "./FormPosicao/FormPosicao";
+import { Container, Row, Col } from "react-bootstrap";
 
 function App() {
   return (
-    <>
-    <SearchBox/>
-    <Maps center={[-1.437972, -48.466901]} positions={[[-1.437972, -48.466901], [-1.44, -48.466901]]}/>
-    </>
+    <Container fluid style={{ width: "1024px" }} className="mt-5">
+      <h2 className="mb-5">Consultar Posição</h2>
+      <Row>
+        <Col>
+          <MapPosicao
+            center={[-1.437972, -48.466901]}
+            positions={[
+              [-1.437972, -48.466901],
+              [-1.44, -48.466901],
+            ]}
+          />
+        </Col>
+        <Col>
+          <FormPosicao />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
